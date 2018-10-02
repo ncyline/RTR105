ee245b1059c56580852807664c984611033c3f6b
user <rinalds17@inbox.lv>
2018-10-02 13:54:40 +0300

    20181002_13_51

---
+++ dgr_20181002.py
@@ -0,0 +1,26 @@
+Python 2.7.12 (default, Dec  4 2017, 14:50:18) 
+[GCC 5.4.0 20160609] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> print(float(99)+100)
+199.0
+>>> print("class")
+class
+>>> ddd= 1+4
+>>> print(ddd)
+5
+>>> vars()
+{'__builtins__': <module '__builtin__' (built-in)>, '__package__': None, '__name__': '__main__', '__doc__': None, 'ddd': 5}
+>>> x=12.2
+>>> y=14
+>>> x=
+SyntaxError: invalid syntax
+>>> print(x)
+12.2
+>>> a=9
+>>> b=2
+>>> c^2=3
+SyntaxError: can't assign to operator
+>>> c=a/b
+>>> print(c)
+4
+>>> 
